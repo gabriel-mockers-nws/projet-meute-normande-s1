8ff6504060800e305fd992b457d87f1c30d9d494
Kimberley Lefebvre <klefebvre@normandiewebschool.fr>
2024-12-10 11:36:15 +0100

    ajout des logo des réseaux dans le footer

--- elements/footer.php
+++ elements/footer.php
@@ -2,6 +2,10 @@
 
 <footer>
     <div class="copyright">©"La Meute Normande", Mai 2022 - Décembre 2024. TOUS DROITS RÉSERVÉS.</div>
+            <div>
+                <img src="/images/instagram-petit.png" alt="instagram"/>
+                <img src="/images/discord-petit.png" alt="discord">
+            </div>
 
 </footer> 
 </body>
