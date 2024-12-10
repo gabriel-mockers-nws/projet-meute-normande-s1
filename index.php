ec93ae5328923e659c8509f1077b33d248cf6712
Kylian Varin <kvarin@normandiewebschool.fr>
2024-12-10 17:08:56 +0100

    suppression hello world

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require_once 'elements/header.php'; ?>
-<p>hello world</p>
 
-  <img class="iamge-bannière" src="images/IMG_4448_m.JPG" alt="Photo meute" height="100%">
+
+  <img class="image-bannière" src="images/IMG_4448_m.JPG" alt="Photo meute" height="100%">
 
 
 
