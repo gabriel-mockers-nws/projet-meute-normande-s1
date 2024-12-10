7933ce20c27512fef5bc9f0d362b1e1c37facb38
Kimberley Lefebvre <klefebvre@normandiewebschool.fr>
2024-12-10 11:43:19 +0100

    ajout des liens vers les réseaux footer

--- elements/footer.php
+++ elements/footer.php
@@ -3,8 +3,8 @@
 <footer>
     <div class="copyright">©"La Meute Normande", Mai 2022 - Décembre 2024. TOUS DROITS RÉSERVÉS.</div>
             <div>
-                <img src="/images/instagram-petit.png" alt="instagram"/>
-                <img src="/images/discord-petit.png" alt="discord">
+                <a href=""><img src="/images/instagram-petit.png" alt="instagram"/></a>
+                <a href=""><img src="/images/discord-petit.png" alt="discord"></a>
             </div>
 
 </footer> 
