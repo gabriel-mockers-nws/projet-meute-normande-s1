9cad56c604a00fae769b5662dc70f89cb9c0af8f
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2024-12-10 17:17:09 +0100

    nav_item function creat for navbar

---
+++ functions/functions.php
@@ -0,0 +1,13 @@
+<?php 
+function nav_item(string $linkClass = ''):string 
+{
+    return 
+    nav_item('../pages/index.php', 'Accueil', $linkClass) . 
+    nav_item('../pages/description.php', 'Qui sommes-nous?', $linkClass) . 
+    nav_item('../pages/evenements.php', 'Evenement', $linkClass) . 
+    nav_item('../pages/galery.php', 'Galerie', $linkClass) .
+    nav_item('../pages/use-link', 'Liens utiles', $linkClass) ;
+}
+
+
+?>
