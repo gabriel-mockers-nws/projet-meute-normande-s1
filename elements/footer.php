fa834e2ba06d320362f5e2abacdf32f35d33e72d
Kimberley Lefebvre <klefebvre@normandiewebschool.fr>
2024-12-10 11:46:06 +0100

    ajout lien vers la page insta footer

--- elements/footer.php
+++ elements/footer.php
@@ -3,7 +3,7 @@
 <footer>
     <div class="copyright">©"La Meute Normande", Mai 2022 - Décembre 2024. TOUS DROITS RÉSERVÉS.</div>
             <div>
-                <a href=""><img src="/images/instagram-petit.png" alt="instagram"/></a>
+                <a href="https://www.instagram.com/la.meute_normande/"><img src="/images/instagram-petit.png" alt="instagram"/></a>
                 <a href=""><img src="/images/discord-petit.png" alt="discord"></a>
             </div>
 
