c64ae403607125e4e667f819ffdce3f62c4accc7
Kimberley Lefebvre <klefebvre@normandiewebschool.fr>
2024-12-10 12:15:31 +0100

    mise en page du code footer

--- elements/footer.php
+++ elements/footer.php
@@ -3,8 +3,12 @@
 <footer>
     <div class="copyright">©"La Meute Normande", Mai 2022 - Décembre 2024. TOUS DROITS RÉSERVÉS.</div>
             <div>
-                <a href="https://www.instagram.com/la.meute_normande/"><img src="/images/instagram-petit.png" alt="instagram"/></a>
-                <a href=""><img src="/images/discord-petit.png" alt="discord"></a>
+                <a href="https://www.instagram.com/la.meute_normande/">
+                    <img src="/images/instagram-petit.png" alt="instagram"/>
+                </a>
+                <a href="">
+                    <img src="/images/discord-petit.png" alt="discord"/>
+                </a>
             </div>
 
 </footer> 
