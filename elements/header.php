9d7b18ebef8517c7eaccbdcea5344d81687c8c4e
Kylian Varin <kvarin@normandiewebschool.fr>
2024-12-12 16:05:44 +0100

    bannière taille

--- elements/header.php
+++ elements/header.php
@@ -22,7 +22,7 @@
         <div class="header-general">
             <div classclass="boxe-header-logo">
                 <a href="../index.php" class="header-logo">
-                <img src="../images/la meute normandemodif 2 clean.png" alt="Logo de la meute" height="100">
+                <img class="header-logo" src="../images/la meute normandemodif 2 clean.png" alt="Logo de la meute" >
                 </a>
             </div>
             <div class="header-nav">
