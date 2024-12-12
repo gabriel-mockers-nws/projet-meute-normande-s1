de1e6a1378578bf505a7d92c6903b57ee10ffa95
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2024-12-12 15:00:09 +0100

    erase commentary on functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -1,7 +1,10 @@
 <?php 
+//fonction pour les item de navigation (a faire)
+
 //fonction pour le menu de navigation 
-function nav_item(string $linkClass = ''):string 
+function nav_item(string $linkClass = ''):string //on créer la fonction 
 {
+    //on retourne les valeur des page en cliquable
     return 
     nav_item('../pages/index.php', 'Accueil', $linkClass) . 
     nav_item('../pages/description.php', 'Qui sommes-nous?', $linkClass) . 
@@ -9,6 +12,5 @@ function nav_item(string $linkClass = ''):string
     nav_item('../pages/galery.php', 'Galerie', $linkClass) .
     nav_item('../pages/use-link', 'Liens utiles', $linkClass) ;
 }
-
 
 ?>
