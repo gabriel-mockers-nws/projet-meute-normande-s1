0ce16b45ab06641a44a94fecdd91bc22b8b387c9
Kylian Varin <kvarin@normandiewebschool.fr>
2024-12-13 21:02:03 +0100

    evenement

--- pages/galery.php
+++ pages/galery.php
@@ -10,4 +10,25 @@ require_once __DIR__ . '/../elements/header.php' ?>
         Ici tu peux observer toutes nos magnifiques photos toutes prises par des talentueux photographes. Elle sont classées par date et par convention. 
     </p>
 </article>
+<div class="colonne-galerie">
+<div>
+    <nav>
+        <a href="">2025</a>
+        <a href="">2024</a>
+        <a href="">2023</a>
+    </nav>
+</div>
+
+<div class="banniere-galerie">
+    <div class=""></div><h1> Galerie </h1>
+    <p>Vous pourrez retrouver ici l'ensemble des photo de la meute lors de nos évènemment !!</p>
+</div>
+
+<div>
+    <p>shdfgqsdhjgfsdhg</p>
+</div>
+
+
+</div>
+
 <?php require_once  __DIR__ . '/../elements/footer.php' ?> 
