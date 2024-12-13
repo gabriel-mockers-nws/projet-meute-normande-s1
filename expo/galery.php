139aee4c12a5bb6e418d716269f91dc6441dadcd
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2024-12-13 15:51:45 +0100

    premier paragraphe dans galey (qui à été déplacée dans le dossier expo

--- expo/galery.php
+++ expo/galery.php
@@ -1,5 +1,13 @@
 <?php
 $title = "Galerie";
 require_once __DIR__ . '/../elements/header.php' ?>
+<h1>
+    Notre galerie photo 
+</h1>
 
+<article>
+    <p>
+        Ici tu peux observer toutes nos magnifiques photos toutes prises par des talentueux photographes. Elle sont classées par date et par convention. 
+    </p>
+</article>
 <?php require_once  __DIR__ . '/../elements/footer.php' ?> 
