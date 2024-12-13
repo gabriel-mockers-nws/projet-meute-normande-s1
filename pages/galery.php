2dbff7e353e4883b247fc6a28b7fab4f4a8281c2
Kylian Varin <kvarin@normandiewebschool.fr>
2024-12-13 22:13:36 +0100

    galerie height

--- pages/galery.php
+++ pages/galery.php
@@ -1,34 +1,29 @@
 <?php
 $title = "Galerie";
 require_once __DIR__ . '/../elements/header.php' ?>
-<h1>
-    Notre galerie photo 
-</h1>
 
-<article>
-    <p>
-        Ici tu peux observer toutes nos magnifiques photos toutes prises par des talentueux photographes. Elle sont classées par date et par convention. 
-    </p>
-</article>
+
 <div class="colonne-galerie">
-<div>
-    <nav>
-        <a href="">2025</a>
-        <a href="">2024</a>
-        <a href="">2023</a>
-    </nav>
+    <div>
+        <nav>
+            <a href="">2025</a>
+            <a href="">2024</a>
+            <a href="">2023</a>
+        </nav>
+    </div>
+
+    
+    <div class="galerie-liste-photo">
+        <h1> Galerie </h1>
+        <p>Vous pourrez retrouver ici l'ensemble des photo de la meute lors de nos évènemment !!</p>
+    </div>
+
+    <div>
+        <p>shdfgqsdhjgfsdhg</p>
+    </div>
+
+
 </div>
 
-<div class="banniere-galerie">
-    <div class=""></div><h1> Galerie </h1>
-    <p>Vous pourrez retrouver ici l'ensemble des photo de la meute lors de nos évènemment !!</p>
-</div>
-
-<div>
-    <p>shdfgqsdhjgfsdhg</p>
-</div>
-
-
-</div>
 
 <?php require_once  __DIR__ . '/../elements/footer.php' ?> 
