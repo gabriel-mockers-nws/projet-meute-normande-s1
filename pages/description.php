8aacc92a8ee1bfd2be86661a510886a55ebf4cba
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2024-12-16 12:19:27 +0100

    description of fandom furry

--- pages/description.php
+++ pages/description.php
@@ -16,9 +16,32 @@ require_once  __DIR__ . '/../elements/header.php' ?>
 <section class="furry-section">
     <h1>Qu'est-ce que le fandom furry </h1>
 
-    <p></p>
-    <p></p>
-    <p></p>
+
+    <h3>Présentation</h3>
+
+    <p>Le furry, c'est se glisser dans la peau (ou plutôt la fourrure !) de personnages anthropomorphes, des créatures mélangeant traits d'animaux et comportements humains. Ces personnages, appelés fursonas, sont souvent des créations personnelles, ce qui offre aux participants une immense liberté d'expression. </p>
+    <br>
+
+    <h3>L'art de se créer</h3>
+
+    <p>Au cœur du cosplay furry se trouvent les fursuits — des costumes confectionnés sur mesure — et les fursonas, des personnages uniques souvent inspirés d’animaux réels ou imaginaires. Chaque fursona permet à son créateur de révéler une facette de sa personnalité ou d'explorer des aspects plus profonds de son identité, tout en s’amusant à incarner un être original.</p>
+    <br>
+
+    <h3>Une Communauté Tolérante et Inclusive</h3>
+
+    <p>Le cosplay furry est bien plus qu'une simple activité artistique : c'est un espace où la tolérance et l’inclusion sont primordiales. Chacun peut y exprimer librement sa créativité et son identité sans crainte de jugement. Pour beaucoup, cette communauté devient un refuge, un lieu où l’on peut être soi-même et tisser des liens authentiques avec d'autres passionnés.</p>
+
+    <br>
+
+    <h3>Conventions : Le Moment de Célébrer Ensemble </h3>
+
+    <p>Des événements comme l’Anthrocon ou l'Eurofurence rassemblent chaque année des milliers de furries du monde entier. C'est l'occasion de montrer ses costumes, de participer à des défilés et de partager des moments inoubliables dans une ambiance chaleureuse et festive. Ces événements sont des moments de grande camaraderie et d'expression artistique, avec également le "dealer's den" où l'on peut acheter des dessins, arts, goodies en tout genre, partie de fursuit, ou même des fursuit dite "adoptable" (déjà faite, on achète également le personnage au créateur). Mais aussi en tant que fursuiteur ou en tant qu’helpeur.</p>
+
+    <br>
+
+    <h3>En conclusion</h3>
+
+    <p>Le cosplay furry, c'est bien plus qu'un simple hobby. C’est une manière de créer, explorer, et partager dans un univers où la tolérance et l'inclusivité règnent. Que vous soyez un participant en costume, un helpeur ou simplement curieux, cet univers unique offre des possibilités infinies pour s’exprimer et se connecter avec une communauté passionnée et bienveillante.</p>
 </section>
 <section class="how-section">
     <h1>Comment rejoindre la Meute</h1>
