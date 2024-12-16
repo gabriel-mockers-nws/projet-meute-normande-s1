e45f672c823da654038a649832ba587201e3bc06
Kylian Varin <kvarin@normandiewebschool.fr>
2024-12-16 10:43:15 +0100

    galerie class

--- pages/galery.php
+++ pages/galery.php
@@ -4,13 +4,18 @@ require_once __DIR__ . '/../elements/header.php' ?>
 
 
 <div class="colonne-galerie">
-    <div>
-        <h3>Choissisez par année</h3>
-    </div>
+    
     <div class="galerie-premier-colonne">
-            <nav><a href="">2025</a></nav>
-            <nav><a href="">2024</a></nav>
-            <nav><a href="">2023</a></nav>
+        <h3>Choissisez par année</h3>
+        <div class="barre-recherche-galerie"><label for="">Rechercher une convention </label>
+                                            <input placeholder="ex: Yvetot" type="text"  >
+        </div>
+        <div class="nav-galerie">
+            <nav><a href="">Année 2025</a></nav>
+            <nav><a href="">Année 2024</a></nav>
+            <nav><a href="">Année 2023</a></nav>
+        </div>
+
         
     </div>
 
