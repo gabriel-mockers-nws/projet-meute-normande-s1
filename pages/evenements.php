91c8ab84f95906b97ab500d1f806e724a6c35a2e
Kimberley Lefebvre <klefebvre@normandiewebschool.fr>
2024-12-16 12:24:17 +0100

    ajout de divers évènements (commentaire) Mise en page à venir

--- pages/evenements.php
+++ pages/evenements.php
@@ -20,42 +20,43 @@ Ajout potentiel d'un système de like/d'intéressement-->
         </h1>
     </div>
     <div>
-
+        <h3>Janvier</h3>
 
 
         <!-- 
     
     Janvier:
-    - 
+    /
 
     Février:
-    - 
+    /
     
     Mars:
-    - 
+    - Paris Manga et Sci-Fi Show → Les 15 et 16 mars au Parc des Expos, Villepinte (93)
     
     Avril:
-    - North Con Japan Le Havre → 5 et 6 Avril 2025 au Carré des Docks, Le Havre (76)
-    - 
+    - North Con Japan Le Havre → 5 et 6 Avril 2025 au Carré des Docks, Le Havre
+    - Bakanim → 5 et 6 Avril à l'ENSIIE, Évry-Courcouronnes (91)
     
     Mai:
-    - 
+    - Jonetsu → 3 et 4 Avril Les Colonnes / L'Agoreine, Bourg-la-reine (92)
+    - Compiègne Geek Convention → 24 et 25 Mai à la salle expo Le Tigre, Margny-Compiègne (60)
+    - Médiévales de Rouen → Du 29 au 31 mai, Rouen
 
     Juin:
-    - 
+    /
 
     juillet:
-    - 
+    - Japan Expo Paris → Du 3 au 6 Juillet au Parc des expositions, Villepinte (93)
 
     Août:
-    - 
+    - Les Historiques de Robert le Diable → dates non communiquées, Moulineaux
         
     Septembre:
-    - 
+    / 
         
     Octobre:
-    - 
-    - Chibi Rouen → 25 et 26 octobre 2025 au Parc des expositions, Rouen (76)
+    - Chibi Rouen → 25 et 26 Octobre au Parc des expositions, Rouen
     
     Novembre:
     /
