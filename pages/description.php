e9b13ddbf62618d8d55fea80968d8b70d684dc10
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2024-12-16 12:02:48 +0100

    meute description

--- pages/description.php
+++ pages/description.php
@@ -1,4 +1,28 @@
 <?php 
 $title = "Description"; 
-require_once  __DIR__ . '/../elements/header.php' ?> 
+require_once  __DIR__ . '/../elements/header.php' ?>
+
+<section class="meute-section">
+    <H1>QUI SOMMES NOUS ?</H1>
+
+    <p>
+        La meute normande est un groupe furry composé d'amis basé en Normandie. Nous organisons des sorties que ce soit entre nous pour passer un moment ensemble, que ce soit au Lac de Léry-Poses, restaurant, furwalk ou tant d'autres. 
+    </p>
+    <br>
+    <p>
+        Nous faisaons aussi des réunions dans des conventions Japan/manga comme la Yvetot, Chibi Rouen ou tant d'autre (cta : voir evenements), dans lesquelles nous nous retrouvons, fursuiteurs, helpeurs, cosplayeurs, et membre sans poils pour nous amuser, déjeuner, et parader ensemble. En effet lors des conventions, nous faisons toujours une parade en fursuit, prenons des photos avec les petits et grands enfants. 
+    </p>
+</section>
+<section class="furry-section">
+    <h1>Qu'est-ce que le fandom furry </h1>
+
+    <p></p>
+    <p></p>
+    <p></p>
+</section>
+<section class="how-section">
+    <h1>Comment rejoindre la Meute</h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore exercitationem asperiores totam dicta ea possimus aperiam officia, corporis veritatis, corrupti architecto? Harum nulla minus ut iusto repellendus rerum nam consectetur.</p>
+</section>
 <?php require_once  __DIR__ . '/../elements/footer.php' ?> 
