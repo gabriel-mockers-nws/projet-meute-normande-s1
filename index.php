d4fdeafc7745e0aaa45e2ac75e5a45b9ff36f6c5
Kylian Varin <kvarin@normandiewebschool.fr>
2025-01-06 16:56:15 +0100

    instagram

--- index.php
+++ index.php
@@ -9,7 +9,22 @@ require_once 'elements/header.php'; ?>
     <p class="texte-bannière">Un groupe de furry Normand passionné par l'art anthropomorphique</p>
   </div>
   
+  <section class="instagram">
+    <div class="titre-insta"> 
+      <h2> Suivez-nous sur Instagram !!</h2>
+    </div>
+    <div class="feed-insta">
+      <div ><img class="box" src="images/la meute normandemodif 2 clean.png" alt="" ></div>
+      <div ><img class="box" src="images/la meute normandemodif 2 clean.png" alt="" ></div>
+      <div ><img class="box" src="images/la meute normandemodif 2 clean.png" alt=""></div>
+      <div ><img class="box" src="images/la meute normandemodif 2 clean.png" alt=""></div>
+      <div ><img class="box" src="images/la meute normandemodif 2 clean.png" alt=""></div>
+      <div ><img class="box" src="images/la meute normandemodif 2 clean.png" alt="" ></div>
+      
 
+    </div>
+  </section>
+ 
 
 
 <?php require_once 'elements/footer.php'; ?>
